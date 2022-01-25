490ee64278337c0fd83db25536962eadb9529524
John Hunter <hunter.john809@gmail.com>
2022-01-25 11:08:31 -0800

    add js game functions

--- main.js
+++ main.js
@@ -1,9 +1,53 @@
-const moves = ["Rock", "Paper", "Scissors"]
+const moves = ["rock", "paper", "scissors"];
 
 function computerMove () {
-    return moves[Math.floor(Math.random()*moves.length)]
+    return moves[Math.floor(Math.random()*moves.length)];
 }
 
-console.log(computerMove());
-console.log(computerMove());
-console.log(computerMove());
+function getPlayerMove () {
+    let playerInput = prompt("Enter move ('rock', 'paper', or 'scissors'): ").toLowerCase().trim();
+    while (!(playerInput === "rock" || playerInput === 'paper' || playerInput === 'scissors')) {
+        playerInput = prompt(`Invalid input: ${playerInput}. Enter move ('rock', 'paper', or 'scissors'): `).toLowerCase().trim();
+    }
+
+    return playerInput;
+}
+
+function playRound (playerMove, computerMove) {
+    alert (`Computer plays: ${computerMove}`)
+    if (playerMove === "rock") {
+        if (computerMove === "rock"){
+            return "tie"
+        }
+        if (computerMove === "paper") {
+            return "computer wins"
+        }
+        else {
+            return "player wins"
+        }
+    }
+    if (playerMove === "paper") {
+        if (computerMove === "rock"){
+            return "player wins"
+        }
+        if (computerMove === "paper") {
+            return "tie"
+        }
+        else {
+            return "computer wins"
+        }
+    }
+    if (playerMove === "scissors") {
+        if (computerMove === "rock"){
+            return "computer wins"
+        }
+        if (computerMove === "paper") {
+            return "player wins"
+        }
+        else {
+            return "tie"
+        }
+    }
+}
+
+alert(playRound(getPlayerMove(), computerMove()));
